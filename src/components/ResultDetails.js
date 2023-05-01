c44f71b3950e160fb5707b84a445cf330ed6ff4f
hiteshri-modi <hiteshrinmodi@gmail.com>
2023-05-01 07:51:14 +0530

    hi

--- src/components/ResultDetails.js
+++ src/components/ResultDetails.js
@@ -1,21 +1,28 @@
 import React from "react";
-import {View, Text, StyleSheet} from 'react-native';
-import { FlatList } from "react-native-gesture-handler";
+import { View, Text, StyleSheet, Image } from "react-native";
 
-const ResultDetails = ({results}) =>{
-    console.log(results.title);
-    return(
-       
-        <View>
-            <Text>
-            result - {results.title}
-            </Text>
-        </View>
-    )
-}
+const ResultDetails = ({ item }) => {
+  return (
+    <View style={styles.container}>
+      <Text> Title : {item.title} </Text>
+      <Image source={{ uri: item.url }} style={styles.photo} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-
+  container: {
+    width: "100%",
+    // alignItems:"center",
+    margin: 30,
+  },
+  photo: {
+    width: "80%",
+    height: 100,
+    borderWidth: 10,
+    marginTop: 5,
+    marginBottom: 20,
+  },
 });
 
 export default ResultDetails;
--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,36 +1,32 @@
 import React from "react";
-import { Text,View,StyleSheet,Image} from "react-native";
+import ResultDetails from "../components/ResultDetails";
+import { View ,Text, StyleSheet} from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
+export default function DetailScreen({ route, navigation }) {
+  const { item } = route.params;
 
-export default function DetailScreen({route, navigation}){
+  return (
+    <View>
+        <ResultDetails item={item}/>
 
-    const {item}= route.params;
+         <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("Home");
+            }}
+          >
+          <Text style={styles.title}> Go Back to HomeScreen</Text>
+          </TouchableOpacity>
+    </View>
     
-    return(
-        <View>
-            <Text>Detail Screen </Text>
-            <Text> Album id : {item.albumId} </Text>
-            <Text>  id : {item.id} </Text>
-            <Text> Title : {item.title} </Text>
-            <Image source={{ uri: item.url }} style={styles.photo} />
-
-            <TouchableOpacity 
-                onPress={() => {navigation.navigate('Home')}}        
-                >
-                <Text style={styles.title}> Go Back to HomeScreen</Text>
-            </TouchableOpacity>
-        </View>
-    );
+  );
 }
+
 const styles = StyleSheet.create({
-    photo: {
-      width: 300,
-      height: 100,
-      borderWidth: 10,
-    },
-    title: {
-      fontSize: 18,
-      fontWeight: "bold",
-    },
-  });
+   title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    margin:20,
+    alignItems:"center",
+    textDecorationLine:"underline"
+  },});
